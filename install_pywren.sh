4b7582e6b0541c088f987664db8d331935a602ac
JosepSampe <josep.sampe@gmail.com>
2018-04-23 13:28:57 +0300

    Updated installation script

--- install_pywren.sh
+++ install_pywren.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#Command: curl -fsSL "https://docs.google.com/uc?export=download&id=10PBhuaPU5YZBpZXm-6I7c12L2qpa40C4" | sh
 echo "Installing PyWren for IBM Cloud Functions..."
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1M4UmpVoEjdyZ3zX9_IlwnVT2HxB0yzRQ' -O pywren_ibm_cloud.tar.gz 2> /dev/null
-tar -xvzf pywren_ibm_cloud.tar.gz > /dev/null
-cd pywren; pip install -U . > /dev/null
+wget --no-check-certificate 'https://github.com/pywren/pywren-ibm-cloud/archive/master.zip' -O pywren_ibm_cloud.zip 2> /dev/null
+unzip  pywren_ibm_cloud.zip > /dev/null
+cd pywren-ibm-cloud-master/pywren; pip install -U . > /dev/null
 echo "done!"
