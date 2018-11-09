a02f6b435acd59223263e6017ccbaa2b14e88ca8
JosepSampe <josep.sampe@gmail.com>
2018-11-09 11:49:22 +0100

    fixed pywrentest.py

--- examples/pywrentest.py
+++ examples/pywrentest.py
@@ -3,10 +3,11 @@ Simple PyWren example using one single function invocation
 """
 import pywren_ibm_cloud as pywren
 
+
 def my_function(x):
     return x + 7
 
 if __name__ == '__main__':
-    pw = pywren.ibm_cf_executor(runtime="pywren_conda_3.6")
+    pw = pywren.ibm_cf_executor()
     pw.call_async(my_function, 3)
     print (pw.get_result())
