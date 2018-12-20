f8d3b4a4bb2d76044d3f05b2234ff18de521c1b3
JosepSampe <josep.sampe@gmail.com>
2018-12-20 15:53:00 +0100

    Updated deploy_runtime script, clone command

--- pywren/pywren_ibm_cloud/storage/backends/cos.py
+++ pywren/pywren_ibm_cloud/storage/backends/cos.py
@@ -15,7 +15,6 @@
 #
 
 import logging
-import requests
 import ibm_boto3
 import ibm_botocore
 from datetime import datetime
@@ -23,6 +22,8 @@ from pywren_ibm_cloud.wrenutil import sizeof_fmt
 from ibm_botocore.credentials import DefaultTokenManager
 from pywren_ibm_cloud.storage.exceptions import StorageNoSuchKeyError
 
+logging.getLogger('ibm_boto3').setLevel(logging.CRITICAL)
+logging.getLogger('ibm_botocore').setLevel(logging.CRITICAL)
 logger = logging.getLogger(__name__)
 
 
@@ -32,19 +33,15 @@ class COSBackend:
     """
 
     def __init__(self, cos_config):
-        logging.getLogger('ibm_boto3').setLevel(logger.getEffectiveLevel())
-        logging.getLogger('ibm_botocore').setLevel(logger.getEffectiveLevel())
-        logger.debug("COSBacked init method")
-        service_endpoint = cos_config.get('endpoint').replace('http:', 'https:')           
+        service_endpoint = cos_config.get('endpoint').replace('http:', 'https:')
 
         if 'api_key' in cos_config:
-            logger.debug("api key found in configuration")
             client_config = ibm_botocore.client.Config(signature_version='oauth',
                                                        max_pool_connections=200,
                                                        user_agent_extra='pywren-ibm-cloud')
             api_key = cos_config.get('api_key')
             token_manager = DefaultTokenManager(api_key_id=api_key)
-            
+
             if 'token' in cos_config:
                 token_manager._token = cos_config['token']
                 expiry_time = cos_config['token_expiry_time']
@@ -57,17 +54,15 @@ class COSBackend:
             if 'token' not in cos_config:
                 cos_config['token'] = token_manager.get_token()
                 cos_config['token_expiry_time'] = token_manager._expiry_time.strftime('%Y-%m-%d %H:%M:%S.%f%z')
-        
+
         elif {'secret_key', 'access_key'} <= set(cos_config):
-            logger.debug("secret_key and access_key found in configuration")
-            
             secret_key = cos_config.get('secret_key')
             access_key = cos_config.get('access_key')
             client_config = ibm_botocore.client.Config(max_pool_connections=200,
                                                        user_agent_extra='pywren-ibm-cloud')
             self.cos_client = ibm_boto3.client('s3',
-                                               aws_access_key_id = access_key, 
-                                               aws_secret_access_key = secret_key,
+                                               aws_access_key_id=access_key,
+                                               aws_secret_access_key=secret_key,
                                                config=client_config,
                                                endpoint_url=service_endpoint)
 
@@ -86,7 +81,7 @@ class COSBackend:
         :type data: str/bytes
         :return: None
         """
-        try:   
+        try:
             res = self.cos_client.put_object(Bucket=bucket_name, Key=key, Body=data)
             status = 'OK' if res['ResponseMetadata']['HTTPStatusCode'] == 200 else 'Error'
             try:
@@ -118,7 +113,7 @@ class COSBackend:
                 raise StorageNoSuchKeyError(key)
             else:
                 raise e
-    
+
     def head_object(self, bucket_name, key):
         """
         Head object from COS with a key. Throws StorageNoSuchKeyError if the given key does not exist.
@@ -127,14 +122,14 @@ class COSBackend:
         :rtype: str/bytes
         """
         try:
-            metadata = self.cos_client.head_object(Bucket=bucket_name, Key=key)       
+            metadata = self.cos_client.head_object(Bucket=bucket_name, Key=key)
             return metadata['ResponseMetadata']['HTTPHeaders']
         except ibm_botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == '404':
                 raise StorageNoSuchKeyError(key)
             else:
                 raise e
-    
+
     def delete_object(self, bucket_name, key):
         """
         Delete an object from storage.
@@ -142,7 +137,7 @@ class COSBackend:
         :param key: data key
         """
         return self.cos_client.delete_object(Bucket=bucket_name, Key=key)
-    
+
     def delete_objects(self, bucket_name, key_list):
         """
         Delete a list of objects from storage.
@@ -152,11 +147,11 @@ class COSBackend:
         result = []
         max_keys_num = 1000
         for i in range(0, len(key_list), max_keys_num):
-            delete_keys = {'Objects' : []}
-            delete_keys['Objects'] = [{'Key' : k} for k in key_list[i:i+max_keys_num]]
+            delete_keys = {'Objects': []}
+            delete_keys['Objects'] = [{'Key': k} for k in key_list[i:i+max_keys_num]]
             result.append(self.cos_client.delete_objects(Bucket=bucket_name, Delete=delete_keys))
         return result
-            
+
     def bucket_exists(self, bucket_name):
         """
         Head bucket from COS with a name. Throws StorageNoSuchKeyError if the given bucket does not exist.
@@ -179,12 +174,12 @@ class COSBackend:
                 page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
             else:
                 page_iterator = paginator.paginate(Bucket=bucket_name)
-                
+
             object_list = []
             for page in page_iterator:
                 if 'Contents' in page:
                     for item in page['Contents']:
-                        object_list.append(item)    
+                        object_list.append(item)
             return object_list
         except ibm_botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == '404':
