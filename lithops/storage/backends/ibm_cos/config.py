ceef146e60cdf8897478f61cd089fa834714f562
JosepSampe <josep.sampe@gmail.com>
2020-11-17 10:10:55 +0100

    reverted cos config

--- lithops/storage/backends/ibm_cos/config.py
+++ lithops/storage/backends/ibm_cos/config.py
@@ -23,9 +23,38 @@ def load_config(config_data):
     if 'ibm_cos' not in config_data:
         raise Exception("ibm_cos section is mandatory in the configuration")
 
-    required_keys_1 = ('region', 'api_key')
-    required_keys_2 = ('region', 'secret_key', 'access_key')
-    required_keys_3 = ('region', 'ibm:iam_api_key')
+    if 'region' in config_data['ibm_cos']:
+        region = config_data['ibm_cos']['region']
+        config_data['ibm_cos']['endpoint'] = PUBLIC_ENDPOINT.format(region)
+
+        if config_data['lithops']['mode'] == 'serverless' \
+           and config_data['serverless']['backend'] == 'ibm_cf':
+            config_data['ibm_cos']['private_endpoint'] = PRIVATE_ENDPOINT.format(region)
+
+        elif config_data['lithops']['mode'] == 'standalone' \
+           and config_data['standalone']['backend'] == 'ibm_vpc':
+            config_data['ibm_cos']['private_endpoint'] = DIRECT_ENDPOINT.format(region)
+
+    if config_data['lithops']['mode'] == 'serverless'\
+       and config_data['serverless']['backend'] == 'ibm_cf':
+        # Private endpoint is mandatory when using IBM CF
+        if 'private_endpoint' not in config_data['ibm_cos']:
+            raise Exception('You must provide the private_endpoint to access to IBM COS')
+        elif 'private' not in config_data['ibm_cos']['private_endpoint']:
+            raise Exception('The private_endpoint you provided to access to IBM COS is not valid')
+        if not config_data['ibm_cos']['private_endpoint'].startswith('http'):
+            raise Exception('IBM COS Private Endpoint must start with http:// or https://')
+
+    elif config_data['lithops']['mode'] == 'standalone' \
+        and config_data['standalone']['backend'] == 'ibm_vpc':
+            pass
+
+    elif 'private_endpoint' in config_data['ibm_cos']:
+        del config_data['ibm_cos']['private_endpoint']
+
+    required_keys_1 = ('endpoint', 'api_key')
+    required_keys_2 = ('endpoint', 'secret_key', 'access_key')
+    required_keys_3 = ('endpoint', 'ibm:iam_api_key')
 
     if 'ibm' in config_data and config_data['ibm'] is not None:
         config_data['ibm_cos'].update(config_data['ibm'])
@@ -37,13 +66,5 @@ def load_config(config_data):
         raise Exception('You must provide {}, {} or {} to access to IBM COS'
                         .format(required_keys_1, required_keys_2, required_keys_3))
 
-    region = config_data['ibm_cos']['region']
-    config_data['ibm_cos']['endpoint'] = PUBLIC_ENDPOINT.format(region)
-
-    if config_data['lithops']['mode'] == 'serverless' \
-       and config_data['serverless']['backend'] == 'ibm_cf':
-        config_data['ibm_cos']['private_endpoint'] = PRIVATE_ENDPOINT.format(region)
-
-    elif config_data['lithops']['mode'] == 'standalone' \
-       and config_data['standalone']['backend'] == 'ibm_vpc':
-        config_data['ibm_cos']['private_endpoint'] = DIRECT_ENDPOINT.format(region)
+    if not config_data['ibm_cos']['endpoint'].startswith('http'):
+        raise Exception('IBM COS Endpoint must start with http:// or https://')
