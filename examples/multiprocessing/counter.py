b712472fab3cc2ec7a69c25344d15877f41258e1
Aitor <aitor.a98@gmail.com>
2020-11-25 11:22:09 +0100

    Updated counter example

--- examples/multiprocessing/counter.py
+++ examples/multiprocessing/counter.py
@@ -1,21 +1,43 @@
-# from multiprocessizzng import Pool, Manager
-from lithops.multiprocessing import Pool, Manager
+# from multiprocessizzng import Pool, Value, RawValue
+from lithops.multiprocessing import Pool, Value, RawValue
 from lithops.utils import setup_logger
-setup_logger('DEBUG')
 
-man = Manager()
-val = man.Value('i', 0)
-lock = man.Lock()
+# setup_logger('DEBUG')
 
 
-def incr(val, lock):
-    with lock:
-        val.value += 1
+def incr(proc_id):
+    for _ in range(100):
+        v.value += 1
 
 
-with Pool() as p:
-    p.starmap(incr, [(val, lock) for _ in range(10)])
-    p.close()
-    p.join()
+def sync_incr(proc_id):
+    for _ in range(100):
+        with v.get_lock():
+            v.value += 1
 
-print(val.value)
+
+if __name__ == '__main__':
+    # Raw shared value
+
+    v = RawValue('i', 0)
+    print(v.value)
+
+    with Pool() as p:
+        res = p.map_async(incr, [str(i) for i in range(4)])
+        p.close()
+        res.get()
+        p.join()
+
+    print(v.value)
+
+    # Synchronized shared value
+    v = Value('i')
+
+    print(v.value)
+    with Pool() as p:
+        res = p.map_async(sync_incr, [str(i) for i in range(4)])
+        p.close()
+        res.get()
+        p.join()
+
+    print(v.value)
