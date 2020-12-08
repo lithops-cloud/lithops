4d980ea46fb1f8ec8247a61d9d2ec73db0c17f14
German Telmo Eizaguirre <gteizaguirre@gmail.com>
2020-12-08 22:25:36 +0100

    minor changes

--- lithops/utils.py
+++ lithops/utils.py
@@ -489,8 +489,6 @@ class WrappedStreamingBodyPartition(WrappedStreamingBody):
             raise EOFError()
         # Data always contain one byte from the previous chunk,
         # so l'ets check if it is a \n or not
-        # self.first_byte = self.sb.read(self.plusbytes)
-        # retval = self.sb.read(n)
         if self.plusbytes != 0:
             self.first_byte = self.sb.read(self.plusbytes)
 
