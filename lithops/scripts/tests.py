35206f49fb1a60ddf64cbe8860aeae4db47d2b11
JosepSampe <josep.sampe@gmail.com>
2021-01-15 18:06:07 +0100

    Add storage arg in lithops console cli

--- lithops/scripts/tests.py
+++ lithops/scripts/tests.py
@@ -529,11 +529,13 @@ def print_help():
         print(f'-> {func_name}')
 
 
-def run_tests(test_to_run, config=None, mode=None, backend=None):
+def run_tests(test_to_run, config=None, mode=None, backend=None, storage=None):
     global CONFIG, STORAGE_CONFIG, STORAGE
 
     mode = mode or get_mode(config)
     config_ow = {'lithops': {'mode': mode}}
+    if storage:
+        config_ow['lithops']['storage'] = storage
     if backend:
         config_ow[mode] = {'backend': backend}
     CONFIG = default_config(config, config_ow)
@@ -565,7 +567,9 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mode', metavar='', default=None,
                         help='serverless, standalone or localhost')
     parser.add_argument('-b', '--backend', metavar='', default=None,
-                        help='serverless, standalone or localhost')
+                        help='compute backend')
+    parser.add_argument('-s', '--storage', metavar='', default=None,
+                        help='storage backend')
     parser.add_argument('-d', '--debug', action='store_true', default=False,
                         help='activate debug logging')
     args = parser.parse_args()
@@ -576,4 +580,4 @@ if __name__ == '__main__':
     if args.test == 'help':
         print_help()
     else:
-        run_tests(args.test, args.config, args.mode, args.backend)
+        run_tests(args.test, args.config, args.mode, args.backend, args.storage)
