8f9f277e7edf7557b0575e2c714a7d3667a8a2ca
Gil Vernik <gilv@il.ibm.com>
2021-01-16 15:19:45 +0200

    [ibm_vpc] update VSI custom image

--- config/ibm_vpc/ubuntu-20.04-server-cloudimg-amd64.sh
+++ config/ibm_vpc/ubuntu-20.04-server-cloudimg-amd64.sh
@@ -34,7 +34,7 @@ virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'sudo rm
 virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'apt-get clean'
 virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'apt-get update >> /tmp/lithops/proxy.log'
 virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'apt-get install unzip python3-pip -y >> /tmp/lithops/proxy.log'
-virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'pip3 install flask gevent pika==0.13.1 ibm-vpc>=0.4.0 namegenerator ibm-cos-sdk requests paramiko python-dateutil>> /tmp/lithops/proxy.log'
+virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'pip3 install flask gevent pika==0.13.1 cloudpickle tblib ps-mem ibm-vpc namegenerator  ibm-cos-sdk requests paramiko python-dateutil>> /tmp/lithops/proxy.log'
 virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y'
 virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - > /dev/null 2>&1 '
 virt-customize -a ubuntu-20.04-server-cloudimg-amd64.img  --run-command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"' 
