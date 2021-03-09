c614dedefe089356ccb63824538f90d2fd387c51
JosepSampe <josep.sampe@gmail.com>
2021-03-09 16:06:13 +0100

    replace cloudpickle

--- lithops/libs/cloudpickle/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if sys.version_info < (3, 8):
-    from .cloudpickle import CloudPickler
-    __version__ = '1.2.2'
-
-else:
-    from .cloudpickle_160_fast import CloudPickler
-    __version__ = '1.6.0'
