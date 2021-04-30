8255d7c543406cba862fa9d0bb17c348a38a6374
JosepSampe <josep.sampe@gmail.com>
2021-04-30 14:02:52 +0200

    remote invoker working

--- lithops/worker/invoker.py
+++ lithops/worker/invoker.py
@@ -14,17 +14,15 @@
 # limitations under the License.
 #
 import os
+import time
 import logging
-import multiprocessing as mp
 from types import SimpleNamespace
-from concurrent.futures import ThreadPoolExecutor
 
 from lithops.serverless import ServerlessHandler
 from lithops.monitor import JobMonitor
 from lithops.storage import InternalStorage
-from lithops.utils import iterchunks
 from lithops.config import extract_serverless_config, extract_storage_config
-from lithops.invokers import FaaSInvoker, create_invoker
+from lithops.invokers import FaaSInvoker
 
 
 logger = logging.getLogger(__name__)
@@ -53,8 +51,7 @@ def function_invoker(job_payload):
     compute_handler = ServerlessHandler(serverless_config, storage_config)
 
     # Create the invokder
-    #invoker = FaaSRemoteInvoker(config, job.executor_id, internal_storage, compute_handler, job_monitor)
-    invoker = create_invoker(config, job.executor_id, internal_storage, compute_handler, job_monitor)
+    invoker = FaaSRemoteInvoker(config, job.executor_id, internal_storage, compute_handler, job_monitor)
     invoker.run_job(job)
 
 
@@ -62,44 +59,20 @@ class FaaSRemoteInvoker(FaaSInvoker):
     """
     Module responsible to perform the invocations against the serverless compute backend
     """
-    ASYNC_INVOKERS = 2
 
-    def __init__(self, config, executor_id, internal_storage, compute_handler, job_monitor):
-        super().__init__(config, executor_id, internal_storage, compute_handler, job_monitor)
-
-        self.job_monitor.token_bucket_q = mp.Queue()
-        self.pending_calls_q = mp.Queue()
-
-    def _invoker_process(self, inv_id):
+    def run_job(self, job):
         """
-        Run process that implements token bucket scheduling approach
+        Run a job
         """
-        logger.info('Invoker process {} started'.format(inv_id))
-        with ThreadPoolExecutor(max_workers=250) as executor:
-            # TODO: Change pending_calls_q check
-            while self.pending_calls_q.qsize() > 0:
-                self.job_monitor.token_bucket_q.get()
-                job, call_ids_range = self.pending_calls_q.get()
-                executor.submit(self._invoke_task, job, call_ids_range)
+        job_monitor = self.job_monitor.create(job, self.internal_storage, generate_tokens=True)
+        self._run_job(job)
+        job_monitor.start()
 
-        logger.info('Invoker process {} finished'.format(inv_id))
+        while self.pending_calls_q.qsize() > 0:
+            time.sleep(1)
 
-    def _invoke_job(self, job):
-        """
-        Run a job described in job_description
-        """
-        for i in range(self.workers):
-            self.job_monitor.token_bucket_q.put('#')
+        job_monitor.stop()  # Stop job monitor thread
+        self.stop()  # Stop async invokers threads
+        time.sleep(5)
 
-        for call_ids_range in iterchunks(range(job.total_calls), job.chunksize):
-            self.pending_calls_q.put((job, call_ids_range))
-
-        invokers = []
-        for inv_id in range(self.ASYNC_INVOKERS):
-            p = mp.Process(target=self._invoker_process, args=(inv_id, ))
-            p.daemon = True
-            p.start()
-            invokers.append(p)
-
-        for p in invokers:
-            p.join()
+        logger.info('Remote Invoker Finished')
