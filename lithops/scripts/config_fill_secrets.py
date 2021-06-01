51cd4ea14fc92a97529d1ed9e869aaf1b8e00050
Cohen-J-Omer <cohen.j.omer@gmail.com>
2021-06-01 14:53:34 +0300

    git action secrets

---
+++ lithops/scripts/config_fill_secrets.py
@@ -0,0 +1,36 @@
+import sys
+
+if __name__ == '__main__':
+    iamapikey, cos_api_key = sys.argv
+
+    with open('lithops/scripts/config_fill_secrets.py', 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace('<iamapikey>', iamapikey)
+    filedata = filedata.replace('<cos_api_key>', cos_api_key)
+
+    with open('file.txt', 'w') as file:
+        file.write(filedata)
+
+
+
+# import argparse
+#
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description="Append secrets to lithops config file",
+#                                      usage='python -m lithops.scripts.config_fill_secrets [-c CONFIG] [-t TESTNAME] ...')
+#     parser.add_argument('-c', '--config', metavar='', default=None,
+#                         help="'path to yaml config file")
+#
+#     args = parser.parse_args()
+#
+#     with open('lithops/scripts/config_fill_secrets.py', 'r') as file:
+#         filedata = file.read()
+#
+#     # Replace the target string
+#     filedata = filedata.replace('<iamapikey>', 'abcd')
+#
+#     # Write the file out again
+#     with open('file.txt', 'w') as file:
+#         file.write(filedata)
