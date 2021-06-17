a8c26a2058410458a1d117811b29ff12949650e7
Cohen-J-Omer <cohen.j.omer@gmail.com>
2021-06-17 19:05:36 +0300

    add a second job

--- lithops/scripts/config_fill_secrets.py
+++ lithops/scripts/config_fill_secrets.py
@@ -1,15 +1,26 @@
+import os
+
 import sys
 
 
+def get_config_files():
+    path = 'lithops/tests/config_files'
+    for file in os.listdir(path):
+        if file.endswith('yaml'):
+            yield file
+
+
 if __name__ == '__main__':
-    iamapikey, cos_api_key = sys.argv[1:]
-    config_file = 'lithops/tests/lithops_config.yaml'
+    iamapikey, cos_api_key, cf_api_key = sys.argv[1:]
 
-    with open(config_file, 'r') as file:
-        filedata = file.read()
+    for config_file in get_config_files():
 
-    filedata = filedata.replace('<iamapikey>', iamapikey).replace('<cos_api_key>', cos_api_key)
+        with open(config_file, 'r') as file:
+            filedata = file.read()
 
-    with open(config_file, 'w') as file:
-        file.write(filedata)
+        filedata = filedata.replace('<iamapikey>', iamapikey).replace('<cos_api_key>', cos_api_key).\
+            replace('<cf_api_key>', cf_api_key)
+
+        with open(config_file, 'w') as file:
+            file.write(filedata)
 
