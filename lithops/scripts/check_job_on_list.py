a0602aeee9357bcb98efb55ba65af6e702d42ca2
Cohen-J-Omer <cohen.j.omer@gmail.com>
2021-06-20 14:18:47 +0300

    filtering workflows

--- lithops/scripts/check_job_on_list.py
+++ lithops/scripts/check_job_on_list.py
@@ -2,16 +2,15 @@ import sys
 
 if __name__ == '__main__':
     workflow = sys.argv[1]  # nightly_build or PR
-    #workflow = 'Push or PR git-action'
+    # workflow = 'Push or PR git-action'
 
     path = ".github/workflows/jobs_to_run.txt"
-    #path_demo = '/Users/omercohen/dev1/lithops_test/.github/workflows/jobs_to_run.txt'
+    # path_demo = '/Users/omercohen/dev1/lithops_test/.github/workflows/jobs_to_run.txt'
 
     with open(path, 'r') as file:
         filedata = file.read()
 
-
     if workflow == 'Push or PR git-action':
-        print(filedata[filedata.find(':')+1:filedata.find('nightly_build')])
+        print(filedata[filedata.find(':') + 1:filedata.find('nightly_build')].replace('\n', ' '))
 
-    #print(True) if job_to_test in filedata else print(False)
+    # print(True) if job_to_test in filedata else print(False)
