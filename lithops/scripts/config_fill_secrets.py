9d145199f0b5e31a5e3a4d89d73845b5e0db8f18
Cohen-J-Omer <cohen.j.omer@gmail.com>
2021-06-20 10:07:36 +0300

    each job will configure its own config file

--- lithops/scripts/config_fill_secrets.py
+++ lithops/scripts/config_fill_secrets.py
@@ -3,24 +3,32 @@ import os
 import sys
 
 
-def get_config_files():
+def get_config_file(backend_name):
     path = 'lithops/tests/config_files/'
     for file in os.listdir(path):
-        if file.endswith('yaml'):
-            yield path + file
+        if file.endswith('yaml') and backend_name in file:
+            return path + file
 
 
 if __name__ == '__main__':
-    iamapikey, cos_api_key, cf_api_key = sys.argv[1:]
+    secrets_to_fill = ['<iamapikey>', '<cos_api_key>', '<cf_api_key>']
+    config_file = get_config_file(sys.argv[1])
+    args = sys.argv[2:]
 
-    for config_file in get_config_files():
+    with open(config_file, 'r') as file:
+        filedata = file.read()
 
-        with open(config_file, 'r') as file:
-            filedata = file.read()
+    for i, arg in enumerate(args):
+        filedata = filedata.replace(secrets_to_fill[i], arg)
 
-        filedata = filedata.replace('<iamapikey>', iamapikey).replace('<cos_api_key>', cos_api_key).\
-            replace('<cf_api_key>', cf_api_key)
+    with open(config_file, 'w') as file:
+        file.write(filedata)
 
-        with open(config_file, 'w') as file:
-            file.write(filedata)
+# filedata = filedata.replace('<iamapikey>', iamapikey).replace('<cos_api_key>', cos_api_key).\
+#     replace('<cf_api_key>', cf_api_key)
 
+# def get_config_files():
+#     path = 'lithops/tests/config_files/'
+#     for file in os.listdir(path):
+#         if file.endswith('yaml'):
+#             yield path + file
