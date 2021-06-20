b124cfa10d322f2ce98c18f9c15e97fd47bcc7a8
Cohen-J-Omer <cohen.j.omer@gmail.com>
2021-06-20 13:01:49 +0300

    filtering workflows

---
+++ lithops/scripts/check_job_on_list.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == '__main__':
+    # workflow = sys.argv[1]  # nightly_build or PR
+    # job_to_test = sys.argv[2]
+    job_to_test = sys.argv[1]
+
+    with open(".github/workflows/jobs_to_run.txt", 'r') as file:
+        filedata = file.read()
+
+    print("true") if job_to_test in filedata else print("false")
