5c8d6df7d040ae03e3f35376b36800eddd1e7822
Cohen-J-Omer <cohen.j.omer@gmail.com>
2021-06-20 15:28:34 +0300

    implementing dynamically named runtime images

--- lithops/scripts/config_fill_secrets.py
+++ lithops/scripts/config_fill_secrets.py
@@ -3,15 +3,17 @@ import os
 import sys
 
 
-def get_config_file(backend_name):
+def get_config_file(backends_name):
+    """returns the lithops config file the contains given backends"""
+
     path = 'lithops/tests/config_files/'
     for file in os.listdir(path):
-        if file.endswith('yaml') and backend_name in file:
+        if file.endswith('yaml') and backends_name in file:
             return path + file
 
 
 if __name__ == '__main__':
-    secrets_to_fill = ['<iamapikey>', '<cos_api_key>', '<cf_api_key>']
+    secrets_to_fill = ['<git-actor>','<iamapikey>', '<cos_api_key>', '<cf_api_key>']
     config_file = get_config_file(sys.argv[1])
     args = sys.argv[2:]
 
