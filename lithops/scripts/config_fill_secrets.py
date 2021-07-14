09b4d5875c0bc8ca8af0c62266ed303457462f62
testing-alt <jackyupjack@gmail.com>
2021-07-14 14:14:21 +0300

    test running CE and CF on two different buckets in parallel

--- lithops/scripts/config_fill_secrets.py
+++ lithops/scripts/config_fill_secrets.py
@@ -13,8 +13,8 @@ def get_config_file(backends_name):
 
 if __name__ == '__main__':
     secrets_to_fill = ['<git-actor>', '<iamapikey>', '<cos_api_key>', '<cf_api_key>']
-    config_file = get_config_file(sys.argv[1])
-    args = sys.argv[2:]
+    config_file = get_config_file(sys.argv[1])  # sys.argv[1] contains the workflow's name
+    args = sys.argv[2:]  # secrets received via a running workflow.
 
     with open(config_file, 'r') as file:
         filedata = file.read()
