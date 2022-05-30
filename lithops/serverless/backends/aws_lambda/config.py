97df7df458b97ddd5f559b99fed04364d24b3967
Damián Maleno <61498645+Damian-MG@users.noreply.github.com>
2022-05-30 13:37:37 +0200

    Update config.py

--- lithops/serverless/backends/aws_lambda/config.py
+++ lithops/serverless/backends/aws_lambda/config.py
@@ -32,7 +32,8 @@ AVAILABLE_PY_RUNTIMES = {
     '3.6': 'python3.6',
     '3.7': 'python3.7' ,
     '3.8': 'python3.8' ,
-    '3.9': 'python3.9'
+    '3.9': 'python3.9' ,
+    '3.10': 'python3.10'
 }
 
 USER_RUNTIME_PREFIX = 'lithops.user_runtimes'
