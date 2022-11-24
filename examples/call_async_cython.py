03e703bf2ac78c014b5b64004202a32347e8d398
JosepSampe <josep.sampe@gmail.com>
2022-11-24 16:54:24 +0200

    update changelog

--- examples/call_async_cython.py
+++ examples/call_async_cython.py
@@ -2,7 +2,7 @@
 Simple Lithops example using one single function invocation
 with a cythonized function located in function.so
 
-Commands to compile the function.py into function.so:
+Commands to compile the function.py into function.so (Ubuntu):
 cython3 -3 --embed -X always_allow_keywords=true -o function.c function.py
 gcc -shared -o function.so -fPIC -I /usr/include/python3.9 function.c
 """
