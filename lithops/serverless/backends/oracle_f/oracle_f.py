d393489435a632b8c367f819ef4b37fe233c442c
Ayman <aymanbourramouss@gmail.com>
2023-05-15 20:17:47 +0200

    Comments and fix lithops verification

--- lithops/serverless/backends/oracle_f/oracle_f.py
+++ lithops/serverless/backends/oracle_f/oracle_f.py
@@ -1,16 +1,16 @@
 import os
 import logging
-import shutil
 import json
-import oci
 import ast
 import hashlib
+import time
 
 from lithops import utils
 from lithops.version import __version__
-from lithops.constants import COMPUTE_CLI_MSG, TEMP_DIR
+from lithops.constants import COMPUTE_CLI_MSG
 
-import time
+
+import oci
 from oci.exceptions import ServiceError
 
 from . import config
@@ -25,40 +25,38 @@ class OracleCloudFunctionsBackend:
         self.name = 'oracle_f'
         self.type = 'faas'
         self.config = oracle_config
+        self.cf_client = self._init_functions_client()
         self.region = oracle_config['region']
         self.tenancy = oracle_config['tenancy']
         self.compartment_id = oracle_config['compartment_id']
         self.user = oracle_config['user']
         self.namespace_name = oracle_config['namespace_name']
-        
-        
         self.default_application_name = f'{config.APPLICATION_NAME}_{self.user[-5:-1].lower()}'
         self.application_name = oracle_config.get('application', self.default_application_name)
-        
-        
-        if 'key_file' in oracle_config and os.path.isfile(oracle_config['key_file']):
-            self.cf_client = oci.functions.FunctionsManagementClient(config=oracle_config)
-        else:
-            signer = oci.auth.signers.get_resource_principals_signer()
-            self.cf_client = oci.functions.FunctionsManagementClient(config={}, signer=signer)
-        
+        self.app_id = self._get_application_id(self.application_name)
+
         msg = COMPUTE_CLI_MSG.format('Oracle Functions')
         logger.info(f"{msg} - Region: {self.region}")
+    
+    def _init_functions_client(self):
+        if 'key_file' in self.config and os.path.isfile(self.config['key_file']):
+            return oci.functions.FunctionsManagementClient(config=self.config)
+        else:
+            self.signer = oci.auth.signers.get_resource_principals_signer()
+            return oci.functions.FunctionsManagementClient(config={}, signer=self.signer)
     
     def _format_function_name(self, runtime_name, runtime_memory, version=__version__):
         name = f'{runtime_name}-{runtime_memory}-{version}'
         name_hash = hashlib.sha1(name.encode("utf-8")).hexdigest()[:10]
-
         return f'lithops-worker-{runtime_name}-v{version.replace(".", "-")}-{name_hash}'
-    
+
     def _unformat_function_name(self, function_name):
         runtime_name, hash = function_name.rsplit('-', 1)
         runtime_name = runtime_name.replace('lithops-worker-', '')
         runtime_name, version = runtime_name.rsplit('-v', 1)
         version = version.replace('-', '.')
         return version, runtime_name
-    
-    
+
     def get_runtime_key(self, runtime_name, runtime_memory, version=__version__):
         """
         Method that creates and returns the runtime key.
@@ -66,8 +64,13 @@ class OracleCloudFunctionsBackend:
         in order to know which runtimes are installed and which not.
         """
         function_name = self._format_function_name(runtime_name, runtime_memory, version)
-        runtime_key = os.path.join(self.name, version, self.region, self.application_name, function_name)
-
+        runtime_key = os.path.join(
+            self.name,
+            version,
+            self.region,
+            self.application_name,
+            function_name
+        )
         return runtime_key
     @staticmethod
     def _create_handler_bin(remove=True):
@@ -91,108 +94,55 @@ class OracleCloudFunctionsBackend:
     def _get_default_runtime_name(self):
         py_version = utils.CURRENT_PY_VERSION.replace('.', '')
         return f'lithops-default-runtime-v{py_version}'
-    
-    def _build_default_runtime(self, runtime_name):
-        """
-        Builds the default runtime
-        """
-        requirements_file = os.path.join(TEMP_DIR, 'oracle_default_requirements.txt')
-        with open(requirements_file, 'w') as reqf:
-            reqf.write(config.REQUIREMENTS_FILE)
-        
-        dockerfile_content = f'''
-            FROM python:{utils.CURRENT_PY_VERSION}-slim
 
-            WORKDIR /app
-
-            COPY {os.path.basename(requirements_file)} ./
-            RUN pip install --no-cache-dir -r {os.path.basename(requirements_file)}
-
-            CMD ["python", "-c", "print('Hello from the Oracle Functions runtime!')"]
-        '''
-        dockerfile_path = os.path.join(TEMP_DIR, 'oracle_default_Dockerfile')
-        shutil.copy(requirements_file, './oracle_default_requirements.txt')
-
-        with open(dockerfile_path, 'w') as dockerfile:
-            dockerfile.write(dockerfile_content)
-
-        try:
-            self.build_runtime(runtime_name, dockerfile_path)
-        finally:
-            print('Cleaning up')
-            
     def clean(self, **kwargs):
         """
         Deletes all runtimes from the current service
         """
         logger.debug('Going to delete all deployed runtimes')
-        
+
         if not self._application_exists(self.application_name):
             return
 
-        app_id = self._get_application_id(self.application_name)
-
-        if app_id is None:
+        if self.app_id is None:
             return
 
-        functions = self.cf_client.list_functions(app_id).data
+        functions = self.cf_client.list_functions(self.app_id).data
 
         for function in functions:
             function_name = function.display_name
-            if function_name.startswith('lithops-worker'):
-                logger.info(f'Going to delete runtime {function_name}')
-                self.cf_client.delete_function(function.id)
+            logger.info(f'Going to delete runtime {function_name}')
+            self.cf_client.delete_function(function.id)
 
-        self.cf_client.delete_application(app_id)
-        
+        self.cf_client.delete_application(self.app_id)
+
     def invoke(self, runtime_name, memory=None, payload={}):
-        
-        logger.debug(f'Extracting runtime metadata from: {runtime_name}')
-
-        # Get the function ID
-        app_id = self._get_application_id(self.application_name)
+        """
+        Invoke function
+        """
+        logger.debug("Extracting runtime metadata from: %s", runtime_name)
         function_name = self._format_function_name(runtime_name, self.config['runtime_memory'])
-        function_ocid = self._get_function_ocid(function_name, app_id)
-
-        # Retrieve the function's information, including the invoke endpoint
-        fn_info = self.cf_client.get_function(function_ocid).data
-
-        # Set the invoke endpoint
-        invoke_endpoint = fn_info.invoke_endpoint
-
-  
-        
-        # Prepare the Oracle Functions client with the invoke endpoint
-        fn_invoke_client = oci.functions.FunctionsInvokeClient(self.config, service_endpoint=invoke_endpoint)
-
-        # Invoke the function with the payload
-        response = fn_invoke_client.invoke_function(
-            function_id=function_ocid,
-            invoke_function_body=json.dumps(payload,default=str),
-            fn_invoke_type='detached'
-        )
-
-        
-
+        response = self.invoke_function(function_name, payload, 'detached')
         status_code = response.status
-
-        if status_code == 200 or status_code == 202:
+        if status_code == 202:
             return response.request_id
         elif status_code == 401:
             logger.debug(response.data.text)
             raise Exception('Unauthorized - Invalid API Key')
         elif status_code == 404:
             logger.debug(response.data.text)
-            raise Exception('Lithops Runtime: {} not deployed'.format(runtime_name))
+            raise Exception(f"Lithops Runtime: {runtime_name} not deployed")
+        else:
+            logger.debug(response.data.text)
+            raise Exception(f"An error occurred: {response.data.text}")
 
-    
     def build_runtime(self, docker_image_name, dockerfile, extra_args=[]):
-        """ Build the runtime Docker image and push it to OCIR. """
-        
+        """ 
+        Build the runtime Docker image and push it to OCIR. 
+        """
         logger.info(f'Building runtime {docker_image_name} from {dockerfile}')
         docker_path = utils.get_docker_path()
 
-        
         # Build the Docker image
         if dockerfile:
             assert os.path.isfile(dockerfile), f'Cannot locate "{dockerfile}"'
@@ -209,7 +159,7 @@ class OracleCloudFunctionsBackend:
 
         cmd = f'{docker_path} tag {docker_image_name}:latest {self.region}.ocir.io/{self.namespace_name}/{docker_image_name}:latest'
         utils.run_command(cmd)
-        
+
         # Push the Docker image to the Oracle Cloud Infrastructure Registry (OCIR)
         cmd = f'{docker_path} push {self.region}.ocir.io/{self.namespace_name}/{docker_image_name}:latest'
         utils.run_command(cmd)
@@ -242,10 +192,9 @@ class OracleCloudFunctionsBackend:
         Checks if a given application exists
         """
         applications = self.cf_client.list_applications(self.config['tenancy']).data
-       
+
         for serv in applications:
             if serv.display_name == application_name:
-                
                 return True
         return False
 
@@ -258,16 +207,46 @@ class OracleCloudFunctionsBackend:
             if serv.display_name == application_name:
                 return serv.id
         return None
+    
+    def invoke_function(self, function_name, payload, invoke_type=None):
+        '''
+        A wrapper for the function invokation API call.
+        '''
+        # Get the function ID
+        function_ocid = self._get_function_ocid(function_name, self.app_id)
+        # Retrieve the function's information, including the invoke endpoint
+        fn_info = self.cf_client.get_function(function_ocid).data
+        # Set the invoke endpoint
+        invoke_endpoint = fn_info.invoke_endpoint
+        # Prepare the Oracle Functions client with the invoke endpoint
+        if 'key_file' in self.config and os.path.isfile(self.config['key_file']):
+            fn_invoke_client = oci.functions.FunctionsInvokeClient(
+                self.config,
+                service_endpoint=invoke_endpoint
+            )
+        else:
+            fn_invoke_client = oci.functions.FunctionsInvokeClient(
+                config={},
+                service_endpoint=invoke_endpoint,
+                signer=self.signer
+            )
+        # Invoke the function with the payload
+        response = fn_invoke_client.invoke_function(
+            function_id=function_ocid,
+            invoke_function_body=json.dumps(payload,default=str),
+            fn_invoke_type=invoke_type
+        )
 
+        return response
 
     def deploy_runtime(self, runtime_name, memory, timeout):
         """
         Deploys a new runtime into Oracle Function Compute
         with the custom modules for lithops
         """
-        logger.info(f"Deploying runtime: {runtime_name} - Memory: {memory} Timeout: {timeout}")
+        logger.info("Deploying runtime: %s - Memory: %s Timeout: %s",runtime_name, memory, timeout)
         if not self._application_exists(self.application_name):
-            logger.debug(f"Creating application {self.application_name}")
+            logger.debug("Creating application %s", self.application_name)
             self.cf_client.create_application(
                 create_application_details=oci.functions.models.CreateApplicationDetails(
                     compartment_id=self.tenancy,
@@ -275,21 +254,14 @@ class OracleCloudFunctionsBackend:
                     subnet_ids=[self.config['vcn']['subnet_ids']]))
 
         function_name = self._format_function_name(runtime_name, memory)
-        app_id = self._get_application_id(self.application_name)
+        logger.debug("Checking if function %s exists", function_name)
 
-        logger.debug(f'Checking if function {function_name} exists')
-        functions = self.cf_client.list_functions(app_id).data
-
-        existing_function = None
-        for function in functions:
-            if function.display_name == function_name:
-                existing_function = function
-                break
+        existing_function = self._get_function_ocid(function_name, self.app_id)
 
         docker_image_name = f'{self.region}.ocir.io/{self.namespace_name}/{runtime_name}:latest'
 
-        if existing_function:
-            logger.debug(f'Function {function_name} already exists. Updating it')
+        if existing_function is not None:
+            logger.debug("Function %s already exists. Updating it", function_name)
             self.cf_client.update_function(
                 function_id=existing_function.id,
                 update_function_details=oci.functions.models.UpdateFunctionDetails(
@@ -297,70 +269,45 @@ class OracleCloudFunctionsBackend:
                     memory_in_mbs=memory,
                     timeout_in_seconds=timeout))
         else:
-            logger.debug(f'Creating function {function_name}')
+            logger.debug("Creating function %s", function_name)
             self.cf_client.create_function(
                 create_function_details=oci.functions.models.CreateFunctionDetails(
                     display_name=function_name,
-                    application_id=app_id,
+                    application_id=self.app_id,
                     image=docker_image_name,
                     memory_in_mbs=memory,
                     timeout_in_seconds=timeout))
-            
+
             max_retries = 5
             retry_interval = 5
-              
+
             for i in range(max_retries):
                 try:
                     metadata = self._generate_runtime_meta(function_name)
                     return metadata
                 except ServiceError as e:
                     if e.status == 404 and i < max_retries - 1:
-                        logger.debug(f"Function not found yet, waiting {retry_interval} seconds before retrying...")
+                        logger.debug("Function not found yet, waiting %s seconds before retrying...", retry_interval)
                         time.sleep(retry_interval)
                     else:
                         raise e
 
-        
+
         metadata = self._generate_runtime_meta(function_name)
         return metadata
 
     def _generate_runtime_meta(self, function_name):
-        logger.debug(f'Extracting runtime metadata from: {function_name}')
-
-        # Get the function ID
-        app_id = self._get_application_id(self.application_name)
-        function_ocid = self._get_function_ocid(function_name, app_id)
-
-        # Retrieve the function's information, including the invoke endpoint
-        fn_info = self.cf_client.get_function(function_ocid).data
-
-        # Set the invoke endpoint
-        invoke_endpoint = fn_info.invoke_endpoint
-
-        # Prepare the Oracle Functions client with the invoke endpoint
-        fn_invoke_client = oci.functions.FunctionsInvokeClient(self.config, service_endpoint=invoke_endpoint)
-
-
-        # Invoke the function with the payload
-        response = fn_invoke_client.invoke_function(
-            function_id=function_ocid,
-            invoke_function_body=json.dumps({"get_metadata": True},default=str)
-        )
-
+        logger.debug("Extracting runtime metadata from: %s", function_name)
+        response = self.invoke_function(function_name, {"get_metadata": True})
         meta_dict = ast.literal_eval(response.data.text)
         result = json.dumps(meta_dict)
         result = json.loads(result)
         if 'lithops_version' in result:
             return result
-        else:
-            raise Exception('An error occurred: {}'.format(result))
-
-
+    
     def _get_function_ocid(self, runtime_name, app_id):
-       
-
         if app_id is None:
-            raise Exception(f'Application {runtime_name} not found.')
+            raise Exception("Application %s not found.", app_id)
 
         # Get the list of functions within the found application
         functions = self.cf_client.list_functions(app_id).data
@@ -369,5 +316,4 @@ class OracleCloudFunctionsBackend:
         for function in functions:
             if function.display_name == runtime_name:
                 return function.id
-
-        raise Exception(f'Function {runtime_name} not found.')
+        raise Exception("Function %s not found.", runtime_name)
--- lithops/storage/backends/oracle_oss/oracle_oss.py
+++ lithops/storage/backends/oracle_oss/oracle_oss.py
@@ -1,5 +1,5 @@
 import oci
-import shutil
+import io
 import os
 import logging
 from oci.object_storage import ObjectStorageClient
@@ -18,69 +18,87 @@ class OCIObjectStorageBackend:
         logger.info("Creating Oracle Object Storage Service client")
         self.config = config
         self.region_name = config['region']
-        
-        if 'key_file' in config and os.path.isfile(config['key_file']):
-            self.object_storage_client = ObjectStorageClient(config)
-        else:
-            signer = oci.auth.signers.get_resource_principals_signer()
-            self.object_storage_client = ObjectStorageClient(config={}, signer=signer)
-        
+        self.object_storage_client = self._init_functions_client()
         self.namespace = self.object_storage_client.get_namespace().data
 
         msg = STORAGE_CLI_MSG.format('Oracle Object Storage')
         logger.info(f"{msg} - Region: {self.region_name}")
+    
+    def _init_functions_client(self):
+        if 'key_file' in self.config and os.path.isfile(self.config['key_file']):
+            return ObjectStorageClient(self.config)
+        else:
+            self.signer = oci.auth.signers.get_resource_principals_signer()
+            return ObjectStorageClient(config={}, signer=self.signer)
     
     def get_client(self):
         return self
 
     def put_object(self, bucket_name, key, data):
         '''
-        Put an object in OCI Object Storage. Override the object if the key already exists.
-        :param bucket_name: name of the bucket.
-        :param key: key of the object.
-        :param data: data of the object
-        :type data: str/bytes
-        :return: None
+        Uploads data to OCI Object Storage with a specified key. Throws StorageNoSuchKeyError if the key does not exist.
+
+        :param bucket_name: The name of the bucket to which the object will be uploaded.
+        :param key: The key under which the object will be stored.
+        :param data: The data to be uploaded, either as a byte string or a BytesIO object.
+
+
+        :raises StorageNoSuchKeyError: If the specified key does not exist in the bucket.
         '''
-        
-        if isinstance(data, str):
-            data = data.encode()
+        # Check if data is a BytesIO object
+        if isinstance(data, io.BytesIO):
+            data = data.getvalue()
 
         try:
             self.object_storage_client.put_object(self.namespace, bucket_name, key, data)
-            
+            logger.debug('OSS Object {} uploaded to bucket {} - Size: {}'.format(key, bucket_name, sizeof_fmt(len(data))))
         except oci.exceptions.ServiceError as e:
             logger.debug("ServiceError in put_object: %s", str(e))
             raise StorageNoSuchKeyError(bucket_name, key)
+
            
 
     def get_object(self, bucket_name, key, stream=False, extra_get_args={}):
         '''
-        Get object from OCI Object Storage with a key. Throws NoSuchKey if the given key does not exist.
-        :param config: OCI config object
-        :param namespace: Object storage namespace
-        :param bucket_name: Bucket name
-        :param key: Key of the object
-        :param stream: Whether to return a stream or read the data
-        :param extra_get_args: Additional arguments for get_object
+        Get object from OCI Object Storage with a key. Throws StorageNoSuchKeyError if the given key does not exist.
+        :param key: key of the object
+        :param key: key of the object
         :return: Data of the object
         :rtype: str/bytes
         '''
-        
+        data = None
         try:
-            r = self.object_storage_client.get_object(self.namespace, bucket_name, key, **extra_get_args)
+            if 'Range' in extra_get_args:
+                range = extra_get_args['Range']
+                r = self.object_storage_client.get_object(self.namespace, bucket_name, key, range=range)
+            else:
+                r = self.object_storage_client.get_object(self.namespace, bucket_name, key)
+
             if stream:
-                data = r.data
+                data = io.BytesIO(r.data.content)
             else:
                 data = r.data.content
-            
-            return data
         except oci.exceptions.ServiceError as e:
-            raise StorageNoSuchKeyError(bucket_name, key)
+            if e.status == 404:
+                raise StorageNoSuchKeyError(bucket_name, key)
+            else:
+                raise e
+        return data
            
         
 
     def upload_file(self, file_name, bucket, key=None, extra_args={}):
+        '''
+        Uploads a file to OCI Object Storage. The file is read in binary mode and uploaded under a specified key.
+        If no key is provided, the base name of the file is used as the key.
+
+        :param file_name: Name of the bucket
+        :param bucket: The name of the bucket to which the file will be uploaded
+        :param key: The key under which the file will be stored. If None, the base name of the file is used
+        :param (Optional) extra_args: Additional arguments that may be passed to the function. Currently unused
+        :return: True if the file was successfully uploaded, False otherwise
+        :rtype: bool
+        '''
         if key is None:
             key = os.path.basename(file_name)
 
@@ -94,6 +112,15 @@ class OCIObjectStorageBackend:
     
 
     def download_file(self, bucket, key, file_name=None, extra_args={}):
+        '''
+        Download a file from the specified bucket and key in the object storage.
+        :param bucket: Name of the bucket
+        :param key: The key or path of the file in the object storage
+        :param file_name: (Optional) The name of the file to be saved locally. If not provided, the key is used as the file name
+        :param extra_args: (Optional) Additional arguments for the download process.
+        :return: True if the file is downloaded successfully, False otherwise.
+        :rtype: bool
+        '''
         if file_name is None:
             file_name = key
 
@@ -111,7 +138,17 @@ class OCIObjectStorageBackend:
         return True
 
     def head_object(self, bucket_name, key):
-        
+        '''
+        Downloads a file from OCI Object Storage. The file is identified by a specified key and is written locally 
+        in binary mode. If no local file name is provided, the key is used as the local file name.
+
+        :param bucket: Name of the bucket
+        :param key: The key under which the file is stored
+        :param (Optional) file_name: The local file path where the downloaded file will be written. If None, the key is used
+        :param extra_args: Additional arguments that may be passed to the function
+        :return: True if the file was successfully downloaded
+        :rtype: bool
+        '''
         try:
             headobj = self.object_storage_client.head_object(self.namespace, bucket_name, key).headers
             return headobj
@@ -120,25 +157,63 @@ class OCIObjectStorageBackend:
 
     
     def delete_object(self, bucket_name, key):
+        '''
+        Deletes an object from OCI Object Storage. The object is identified by a specified key in a specified bucket.
+
+        :param bucket_name: Name of the bucket
+        :param key: The key under which the object is stored
+
+        '''
         self.object_storage_client.delete_object(self.namespace, bucket_name, key)
     
     def delete_objects(self, bucket_name, keys_list):
+        '''
+        Deletes multiple objects from OCI Object Storage. The objects are identified by a list of keys in a specified bucket.
+
+        :param bucket_name: Name of the bucket
+        :param keys_list: A list of keys under which the objects are stored
+        '''
         for key in keys_list:
             self.object_storage_client.delete_object(self.namespace, bucket_name, key)
 
     def head_bucket(self, bucket_name):
+        '''
+        Return the metadata for a bucket from OCI Object Storage. 
+        :param bucket_name: Name of the bucket
+        :return: A dictionary containing the HTTP status code and headers for the bucket
+        :rtype: dict
+        :raises StorageNoSuchKeyError: If the specified bucket does not exist
+        '''
+        response = {
+            'ResponseMetadata':
+                {'HTTPStatusCode': 200,
+                 'HTTPHeaders': {'content-type': 'application/xml',
+                                 'server': 'OracleStorage'}}
+        }
         try:
             metadata = self.object_storage_client.head_bucket(self.namespace, bucket_name)
-            return vars(metadata)
+            response['ResponseMetadata']['HTTPStatusCode'] = metadata.status
+            return response
         except oci.exceptions.ServiceError:
             raise StorageNoSuchKeyError(bucket_name,'')
     
-    def list_objects(self, bucket_name, prefix=None):
-        
+    def list_objects(self, bucket_name, prefix=None, match_pattern=None):
+        '''
+        Return the list of objects from OCI Object Storage for the specified bucket.
+
+        :param bucket_name: Name of the bucket
+        :param prefix: (Optional) Prefix to filter object names. Default is None
+        :param match_pattern: (Optional) Match pattern to further filter object names. Default is None
+        :return: A list of dictionaries containing the keys and sizes of the objects that match the given prefix and match pattern
+        :rtype: list of dict
+        :raises StorageNoSuchKeyError: If the specified bucket does not exist or there is a service error
+        '''
         prefix = '' if prefix is None else prefix
         try:
-            res = self.object_storage_client.list_objects(self.namespace, bucket_name,prefix=prefix,limit=1000)
-            obj_list = [obj.name for obj in res.data.objects]
+            res = self.object_storage_client.list_objects(self.namespace, bucket_name, prefix=prefix,limit=1000,fields="name,size")
+            obj_list = [{'Key': obj.name, 'Size': obj.size} for obj in res.data.objects]
+            
+            
             return obj_list
 
         except oci.exceptions.ServiceError as e:
