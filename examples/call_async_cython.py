97d37af8313d9bc67b51fa308682702b04cef411
JosepSampe <josep.sampe@gmail.com>
2023-11-15 15:48:33 +0100

    [Serilaizer] Take into account .so files

--- examples/call_async_cython.py
+++ examples/call_async_cython.py
@@ -1,10 +1,13 @@
 """
-Simple Lithops example using one single function invocation
-with a cythonized function located in function.so
+Simple Lithops example using one single function invocation with a
+cythonized function located in function.cpython-39-x86_64-linux-gnu.so
 
 Commands to compile the function.py into function.so (Ubuntu):
 cython3 -3 --embed -X always_allow_keywords=true -o function.c function.py
 gcc -shared -o function.so -fPIC -I /usr/include/python3.9 function.c
+
+To make this example working, you have to delete funtion.py and rename
+function.cpython-39-x86_64-linux-gnu.so -> function.so
 """
 import lithops
 from function import my_c_function
---
+++ lithops/libs/imp/__init__.py
@@ -0,0 +1,13 @@
+from .imp import (
+    find_module,
+    SEARCH_ERROR,
+    PY_SOURCE,
+    PY_COMPILED,
+    C_EXTENSION,
+    PY_RESOURCE,
+    PKG_DIRECTORY,
+    C_BUILTIN,
+    PY_FROZEN,
+    PY_CODERESOURCE,
+    IMP_HOOK,
+)
